2c6dbaf32d682112adecf8c948d42650d0ef47ba
@itsAj <ajaymati1000@gmail.com>
2020-09-13 17:19:31 +0530

    headerCard added

---
+++ src/components/HeaderCard/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import SummaryCard from '../SummaryCard';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  card1: {
+    padding: "20px",
+    flex: 1,
+  },
+  card2: {
+    padding: "10px",
+    minWidth: "200px",
+  },
+  container1: {
+    width: "100%",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "nowrap",
+    overflowX: "auto",
+    overflowY: "hidden",
+  },
+  container2: {
+    width: "100%",
+    display: "flex",
+    flexWrap: "nowrap",
+    overflowX: "auto",
+    overflowY: "hidden",
+  },
+});
+function HeaderCard() {
+  const matches = useMediaQuery("(min-width:650px)");
+  const classes = useStyles();
+  return (
+    <>
+      <div className={matches ? classes.container1 : classes.container2}>
+        <div className={matches ? classes.card1 : classes.card2}>
+          <SummaryCard
+            title={"Confirmed"}
+            value={7390}
+            subtitle={"new confirmed"}
+            subvalue={123}
+            color={"#fa6400"}
+          />
+        </div>
+        <div className={matches ? classes.card1 : classes.card2}>
+          <SummaryCard
+            title={"Recovered"}
+            value={2000}
+            subtitle={"new recovered"}
+            subvalue={35}
+            color={"#1cb142"}
+          />
+        </div>
+        <div className={matches ? classes.card1 : classes.card2}>
+          <SummaryCard
+            title={"Death"}
+            value={7}
+            subtitle={"new death"}
+            subvalue={1}
+            color={"#f9345e"}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeaderCard;
