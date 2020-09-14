9c347f15dc1bd018ba4a775af6f3653464965fb8
Pragya <divyanshuraj5050@gmail.com>
2020-09-14 02:51:15 -0400

    Made a notification card using react in which we are fetching data from an external source using axios and also for styling bootstrap is used.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/notification/notifications'
+import Notification from './components/notification/index'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    titleDate : [{
+        title : 'title1',
+        date : 'date1',
+        link: 'link'
+    },
+
+]}
+
+  render() { 
+    return ( 
+      <Notification>
+      </Notification>     
+     );
+  }
 }
-
+ 
 export default App;
